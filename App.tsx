921bb1a05808e2dcdf29150598019a81f840ede6
RadianceSA23 <riseinradiance@gmail.com>
2025-07-08 22:30:01 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,33 +1,3 @@
-// import React from 'react';
-// import { Provider } from 'react-redux';
-// import { store } from './src/redux/store';
-// import { StatusBar, useColorScheme, StyleSheet } from 'react-native';
-// import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-// import HomeScreen from './src/screens/HomeScreen';
-
-// const App = () => {
-//   const isDarkMode = useColorScheme() === 'dark';
-// console.log("colorTheme is isDarkMode: "+isDarkMode)
-//   return (
-//     <Provider store={store}>
-//       <SafeAreaProvider>
-//       <SafeAreaView style={[styles.container, { backgroundColor: isDarkMode ? '#000' : '#fff' }]}>
-//         <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-//         <HomeScreen />
-//       </SafeAreaView>
-//       </SafeAreaProvider>
-      
-//     </Provider>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-// });
-
-// export default App;
 
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
